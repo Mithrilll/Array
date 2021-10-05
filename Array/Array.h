939c0e43f7de72a709b6354adc420b6dacd2b102
Mifril <rybalovalex03@gmail.com>
2021-10-05 11:46:56 +0700

    Шаблонный класс

--- Array/Array.h
+++ Array/Array.h
@@ -3,23 +3,26 @@
 #include <random>
 #include <time.h>
 
+template<class T>
 class Array 
 {
 public:
 	Array();
-	Array(int* arr, int n);
+	Array(T* arr, size_t n);
 	Array(const Array& object);
 	~Array();
 
-	int FindElem(int value);
+	size_t FindElem(T value);
 	
-	friend std::ostream& operator<<(std::ostream& out, const Array& arr);	// перегрузка оператора вывода
-	friend std::istream& operator>>(std::istream& in, Array& arr);	// перегрузка оператора ввода
+	template<typename P>
+	friend std::ostream& operator<<(std::ostream& out, const Array<P>& arr);	// перегрузка оператора вывода
+	template<typename P>
+	friend std::istream& operator>>(std::istream& in, Array<P>& arr);	// перегрузка оператора ввода
 
-	void Sort();
-	void Sort(int left, int right);
+	bool Sort();
+	bool Sort(size_t left, size_t right);
 
-	void Insert(int value, int index);
+	void Insert(T value, size_t index);
 
 	void ShiftLeft(int ind, int k);
 	void ShiftRight(int ind, int k);
@@ -27,24 +30,355 @@ public:
 	int Min();
 	int Max();
 
-	void Fill(int low, int high);
+	/*void Fill(int low, int high);
 	void FillAscending(int low, int high);
-	void FillDescending(int low, int high);
+	void FillDescending(int low, int high);*/
 
-	int& operator[](int index);
+	T& operator[](size_t index);
 
-	Array operator=(const Array& object);
+	Array<T> operator=(const Array<T>& object);
 
-	Array operator+(int value);
-	void operator+=(int value);
+	Array<T> operator+(T value);
+	void operator+=(T value);
 
-	void operator-=(int index);
-	Array operator-(int value);
+	void DeleteByIndex(size_t index);
+	void DeleteByValue(T value);
+
+	void operator-=(size_t index);
+	void operator-(T value);
 	
-	bool operator==(const Array& second_operand);	// перегрузка операции ==
-	bool operator!=(const Array& second_operand);	// перегрузка операции !=
+	bool operator==(const Array<T>& second_operand);	// перегрузка операции ==
+	bool operator!=(const Array<T>& second_operand);	// перегрузка операции !=
 
 private:
-	int size;
-	int* pArr;
+	size_t size;
+	T* pArr;
 };
+
+template<typename T>
+Array<T>::Array()
+{
+	size = 0;
+	pArr = nullptr;
+}
+
+template<typename T>
+Array<T>::Array(T* arr, size_t n)
+{
+	size = n;
+	pArr = new T[size];
+	for (size_t i = 0; i < size; i++)
+		pArr[i] = arr[i];
+}
+
+template<typename T>
+Array<T>::Array(const Array<T>& object)
+{
+	size = object.size;
+	pArr = new T[size];
+	for (size_t i = 0; i < size; i++)
+		pArr[i] = object.pArr[i];
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+	if (pArr)
+		delete[] pArr;
+}
+
+template<typename T>
+size_t Array<T>::FindElem(T value)
+{
+	for (size_t i = 0; i < size; i++)
+		if (pArr[i] == value)
+		{
+			return i;
+		}
+
+	return -1;
+}
+
+template<typename P>
+std::ostream& operator<<(std::ostream& out, const Array<P>& arr)
+{
+	for (size_t i = 0; i < arr.size; i++)
+		out << arr.pArr[i] << " ";
+
+	return out;
+}
+
+template<typename P>
+std::istream& operator>>(std::istream& in, Array<P>& arr)
+{
+	for (size_t i = 0; i < arr.size; i++)
+		in >> arr[i];
+
+	return in;
+}
+
+template<typename T>
+bool Array<T>::Sort()
+{
+	return Sort(0, size - 1);
+}
+
+template<typename T>
+bool Array<T>::Sort(size_t left, size_t right)
+{
+	if (left < 0 || right >= size)
+	{
+		return false;
+	}
+
+	int i = left, j = right;
+	T tmp;
+	T pivot = pArr[(left + right) / 2];
+
+	while (i <= j)
+	{
+		while (pArr[i] < pivot)
+			i++;
+		while (pArr[j] > pivot)
+			j--;
+		if (i <= j)
+		{
+			tmp = pArr[i];
+			pArr[i] = pArr[j];
+			pArr[j] = tmp;
+			i++;
+			j--;
+		}
+	}
+
+	if (left < j)
+		Sort(left, j);
+	if (i < right)
+		Sort(i, right);
+
+	return true;
+}
+
+template<typename T>
+void Array<T>::Insert(T value, size_t index)
+{
+	size++;
+	T* temp = new T[size];
+	for (size_t i = 0; i < index; i++)
+		temp[i] = pArr[i];
+	temp[index] = value;
+	for (size_t i = index + 1; i < size; i++)
+		temp[i] = pArr[i - 1];
+
+	delete[] pArr;
+	pArr = temp;
+}
+
+template<typename T>
+void Array<T>::ShiftLeft(int ind, int k)
+{
+	if (ind < 0 || k < 0)
+		return;
+
+	int i = 0;
+	while (i + k <= ind)
+	{
+		pArr[i] = pArr[i + k];
+		i++;
+	}
+	while (i <= ind)
+	{
+		pArr[i] = 0;
+		i++;
+	}
+}
+
+template<typename T>
+void Array<T>::ShiftRight(int ind, int k)
+{
+	if (ind < 0 || k < 0)
+		return;
+
+	size_t i = size - 1;
+	while (i - k >= ind)
+	{
+		pArr[i] = pArr[i - k];
+		i--;
+	}
+	while (i >= ind)
+	{
+		pArr[i] = 0;
+		i--;
+	}
+}
+
+template<typename T>
+int Array<T>::Min()
+{
+	if (!pArr)
+		return 0;
+	T min = pArr[0];
+
+	for (size_t i = 0; i < size; i++)
+		if (pArr[i] < min)
+			min = pArr[i];
+
+	return min;
+}
+
+template<typename T>
+int Array<T>::Max()
+{
+	if (!pArr)
+		return 0;
+	T max = pArr[0];
+
+	for (size_t i = 0; i < size; i++)
+		if (pArr[i] > max)
+			max = pArr[i];
+
+	return max;
+}
+
+//void Array::Fill(int low, int high)
+//{
+//	for (int i = 0; i < size; i++)
+//		pArr[i] = rand() % (high - low + 1) + low;
+//}
+//
+//void Array::FillAscending(int low, int high)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		pArr[i] = rand() % (high - low + 1) + low;
+//		low = pArr[i];
+//	}
+//}
+//
+//void Array::FillDescending(int low, int high)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		pArr[i] = rand() % (high - low + 1) + low;
+//		high = pArr[i];
+//	}
+//}
+
+template<typename T>
+T& Array<T>::operator[](size_t index)
+{
+	return pArr[index];
+}
+
+template<typename T>
+Array<T> Array<T>::operator=(const Array<T>& object)
+{
+	if (*this == object)
+		return *this;
+
+	if (pArr)
+		delete[] pArr;
+
+	size = object.size;
+	pArr = new T[size];
+	for (size_t i = 0; i < size; i++)
+		pArr[i] = object.pArr[i];
+
+	return *this;
+}
+
+template<typename T>
+Array<T> Array<T>::operator+(T value)
+{
+	T* temp = new T[size + 1];
+
+	for (size_t i = 0; i < size; i++)
+		temp[i] = pArr[i];
+
+	temp[size] = value;
+
+	Array n(temp, size + 1);
+	delete[] temp;
+
+	return n;
+}
+
+template<typename T>
+void Array<T>::operator+=(T value)
+{
+	T* temp = new T[size + 1];
+
+	for (size_t i = 0; i < size; i++)
+		temp[i] = pArr[i];
+
+	temp[size] = value;
+
+	size++;
+	delete[] pArr;
+	pArr = temp;
+}
+
+template<class T>
+void Array<T>::DeleteByIndex(size_t index)
+{
+	if (index < 0 || index >= size)
+		return;
+
+	T* temp = new T[size - 1];
+
+	for (size_t i = 0; i < index; i++)
+		temp[i] = pArr[i];
+
+	for (size_t i = index + 1; i < size; i++)
+		temp[i - 1] = pArr[i];
+
+	delete[] pArr;
+	pArr = temp;
+	size--;
+}
+
+template<class T>
+void Array<T>::DeleteByValue(T value)
+{
+	size_t index = -1;
+
+	for (size_t i = 0; i < size; i++)
+		if (pArr[i] == value)
+		{
+			index = i;
+			break;
+		}
+
+	DeleteByIndex(index);
+}
+
+template<typename T>
+void Array<T>::operator-=(size_t index)
+{
+	DeleteByIndex(index);
+}
+
+template<typename T>
+void Array<T>::operator-(T value)
+{
+	DeleteByValue(value);
+}
+
+template<typename T>
+bool Array<T>::operator==(const Array<T>& second_operand)
+{
+	if (size != second_operand.size)
+		return false;
+
+	for (size_t i = 0; i < size; i++)
+		if (pArr[i] != second_operand.pArr[i])
+			return false;
+
+	return true;
+}
+
+template<typename T>
+bool Array<T>::operator!=(const Array<T>& second_operand)
+{
+	return !(*this == second_operand);
+}
--- Array/Source.cpp
+++ Array/Source.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int a[5] = { 1, 5, 3, 6, 2 };
-	Array arr(a, 5);
+	Array<int> arr(a, 5);
 
 	std::cout << "Index of element with value 3 : " << arr.FindElem(3) << std::endl;
 	arr.Sort();
@@ -16,16 +16,10 @@ int main()
 	std::cout << "Shifted right from index 3 by 1 position:\n" << arr << std::endl;
 	std::cout << "Minimum : " << arr.Min() << std::endl;
 	std::cout << "Maximum : " << arr.Max() << std::endl;
-	arr.Fill(5, 25);
-	std::cout << "Filled in range [5, 25]:\n" << arr << std::endl;
-	arr.FillAscending(5, 25);
-	std::cout << "Filled in ascending order in range [5, 25]:\n" << arr << std::endl;
-	arr.FillDescending(5, 25);
-	std::cout << "Filled in descending order in range [5, 25]:\n" << arr << std::endl;
 	std::cout << "Array[0]: " << arr[0] << std::endl;
 	arr = arr + 2;
 	std::cout << "Array + 2:\n" << arr << std::endl;
-	arr = arr - 3;
+	arr - 3;
 	std::cout << "Array - 3:\n" << arr << std::endl;
 	arr -= 2;
 	std::cout << "Array -= 2:\n" << arr << std::endl;
